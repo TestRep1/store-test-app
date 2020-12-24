a075ba3a88318b97069ea71570d5669d15b7f62e
oklamos <oklamos@gmail.com>
2020-12-24 02:02:12 +0200

    add more code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/homepage/homepage';
+
+const Hats = () => (
+  <h1>HATS</h1>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route path="/shop/hats" component={Hats}/>
+      </Switch>
+     
     </div>
   );
 }
